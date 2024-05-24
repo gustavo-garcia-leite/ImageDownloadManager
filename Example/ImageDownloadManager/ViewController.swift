a3aee4a5ad276fb6ff8fedc88011206010931c91
Gustavo Garcia Leite <gustavo031199@gmail.com>
2024-05-23 22:14:02 -0300

    feat: add cocoapods base project

---
+++ Example/ImageDownloadManager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ImageDownloadManager
+//
+//  Created by gustavo-garcia-leite on 05/23/2024.
+//  Copyright (c) 2024 gustavo-garcia-leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/ImageDownloadManager/ImageDownloadManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ImageDownloadManager : NSObject
+@end
+@implementation PodsDummy_ImageDownloadManager
+@end
